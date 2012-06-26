ae67b8b34381d8992141214d06ec568d18b8e9ee
Christian Nørlyng <christian@testtech.dk>
2012-06-26 15:22:52 +0200

    FileStuff
    .

---
+++ src/GrapherTool_src/FilemyUtilities.java
@@ -0,0 +1,37 @@
+package GrapherTool_src;
+
+
+import java.util.*;
+import java.util.Properties;
+import java.io.*;
+import java.io.File; 
+
+public class FilemyUtilities {
+
+	public int FileComp(String FileOne, String FileTwo){
+		  
+		        // Compare two files.   
+		
+		        return FileOne.compareTo(FileTwo);   
+		  
+	}
+	
+	public boolean TestScriptsClean(){
+		boolean Result = false; 
+		
+		  File dir = new File("");
+		   for (File child : dir.listFiles()) {
+
+		     if (".".equals(child.getName()) || "..".equals(child.getName())) {
+
+		       continue;
+		  // Ignore the self and parent aliases.     
+		       
+		     }
+
+		     // Do something with child 
+		 
+		   }
+		   return Result;
+	}
+}
